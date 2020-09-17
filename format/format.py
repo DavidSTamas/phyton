8be5e3c7aaee39fae0ebee3421e259c44a5221e0
DavidSTamas <davidsandor.tamas@gmail.com>
2020-09-17 18:02:24 +0200

    09171802

---
+++ format/format.py
@@ -0,0 +1,15 @@
+
+szam = 35.123
+print("|%-20.2d|" % szam)
+
+szam2 = 3425.1234
+print("|%-20.2f|" % szam2)
+
+nev = "Tamás Dávid"
+print("|%20s|" % nev)
+
+ar = 340480.005
+print("Ár: %.2f Ft" % ar)
+
+betu = "a"
+print("%c" % betu)
---
+++ format/matek.py
@@ -0,0 +1,8 @@
+import math
+
+szam = 9
+print(math.sqrt(szam))
+
+szog = 1
+rad = szog * math.pi / 180
+print(math.sin(rad))
